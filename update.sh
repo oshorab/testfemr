95a87530e11141534de3eda72a5c1a31f08af3bd
oshorab <omarshorab@gmail.com>
2021-06-03 15:29:02 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -24,13 +24,16 @@ if [ -x "$(command -v docker-compose)" ]; then
     echo "Update docker-compose"
 else
     echo "Install docker-compose"
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
+#     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#     sudo chmod +x /usr/local/bin/docker-compose
 fi
 
 docker-compose down
 docker-compose pull
 docker-compose up
+
+docker -v
+docker-compose -v
 
 ## start mysql
 #sudo -S launchctl load -F /Library/LaunchDaemons/com.oracle.oss.mysql.mysqld.plist
