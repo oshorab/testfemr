e6aee8c77b07ceb1c46571b94ad16c235a3a9788
oshorab <omarshorab@gmail.com>
2021-06-02 21:06:27 -0700

    hi

---
+++ app/femr/business/services/system/UpdatesService.java
@@ -0,0 +1,249 @@
+/*
+     fEMR - fast Electronic Medical Records
+     Copyright (C) 2014  Team fEMR
+
+     fEMR is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     fEMR is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with fEMR.  If not, see <http://www.gnu.org/licenses/>. If
+     you have any questions, contact <info@teamfemr.org>.
+*/
+package femr.business.services.system;
+
+import com.google.inject.Inject;
+import femr.business.services.core.IUpdatesService;
+import femr.common.dtos.ServiceResponse;
+import femr.data.daos.IRepository;
+import femr.data.models.core.IKitStatus;
+import femr.data.models.core.INetworkStatus;
+import femr.data.models.core.IDatabaseStatus;
+import femr.data.models.mysql.KitStatus;
+import femr.data.models.mysql.NetworkStatus;
+import femr.data.models.mysql.DatabaseStatus;
+import femr.ui.controllers.BackEndControllerHelper;
+import fr.bmartel.speedtest.SpeedTestReport;
+import fr.bmartel.speedtest.SpeedTestSocket;
+import fr.bmartel.speedtest.inter.ISpeedTestListener;
+import fr.bmartel.speedtest.model.SpeedTestError;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.io.InputStreamReader;
+import java.security.UnresolvedPermission;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UpdatesService implements IUpdatesService {
+
+    private final IRepository<INetworkStatus> networkStatusRepository;
+    private final IRepository<IKitStatus> kitStatusRepository;
+    private final IRepository<IDatabaseStatus> databaseStatusRepository;
+
+    @Inject
+    public UpdatesService(IRepository<INetworkStatus> networkStatusRepository,
+                          IRepository<IKitStatus> kitStatusRepository,
+                          IRepository<IDatabaseStatus> databaseStatusRepository) {
+        this.networkStatusRepository = networkStatusRepository;
+        this.kitStatusRepository = kitStatusRepository;
+        this.databaseStatusRepository = databaseStatusRepository;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ServiceResponse<List<? extends INetworkStatus>> retrieveNetworkStatuses() {
+        ServiceResponse<List<? extends INetworkStatus>> response = new ServiceResponse<>();
+        try {
+            List<? extends INetworkStatus> networkStatuses = networkStatusRepository.findAll(NetworkStatus.class);
+            response.setResponseObject(networkStatuses);
+
+        } catch (Exception ex) {
+            response.addError("", ex.getMessage());
+        }
+        return response;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public  ServiceResponse<List<? extends INetworkStatus>> updateNetworkStatuses() {
+        ServiceResponse<List<? extends INetworkStatus>> response = new ServiceResponse<>();
+        ArrayList<String> data = new ArrayList<>();
+        try {
+            // DOWNLOAD SPEED TEST SOCKET
+            SpeedTestSocket speedTestSocketDownload = new SpeedTestSocket();
+            speedTestSocketDownload.addSpeedTestListener(new ISpeedTestListener() {
+                @Override
+                public void onCompletion(SpeedTestReport report) {
+                    System.out.println("DOWNLOAD [COMPLETED] rate in bit/s   : " + report.getTransferRateBit());
+                    INetworkStatus download = retrieveNetworkStatuses().getResponseObject().get(1);
+                    download.setValue((report.getTransferRateBit().
+                            divide(new BigDecimal(1000000))).setScale(2, RoundingMode.FLOOR).toString() + " mbps");
+                    networkStatusRepository.update(download);
+                }
+
+                @Override
+                public void onProgress(float percent, SpeedTestReport report) { }
+
+                @Override
+                public void onError(SpeedTestError speedTestError, String errorMessage) {
+                    response.addError("Network SpeedTest error", speedTestError.toString());
+                }
+            });
+            speedTestSocketDownload.startFixedDownload("http://ipv4.ikoula.testdebit.info/100M.iso", 5000);
+
+            // UPLOAD SPEED TEST SOCKET
+            SpeedTestSocket speedTestSocketUpload = new SpeedTestSocket();
+            speedTestSocketUpload.addSpeedTestListener(new ISpeedTestListener() {
+                @Override
+                public void onCompletion(SpeedTestReport report) {
+                    System.out.println("UPLOAD [COMPLETED] rate in bit/s   : " + report.getTransferRateBit());
+                    INetworkStatus upload = retrieveNetworkStatuses().getResponseObject().get(2);
+                    upload.setValue((report.getTransferRateBit().
+                            divide(new BigDecimal(1000000))).setScale(2, RoundingMode.FLOOR).toString() + " mbps");
+                    networkStatusRepository.update(upload);
+                }
+
+                @Override
+                public void onProgress(float percent, SpeedTestReport report) { }
+
+                @Override
+                public void onError(SpeedTestError speedTestError, String errorMessage) {
+                    response.addError("Network SpeedTest error", speedTestError.toString());
+                }
+            });
+            speedTestSocketUpload.startFixedUpload("http://ipv4.ikoula.testdebit.info/", 10000000, 5000);
+
+            // TODO need to find a new ping check method
+            Float Ping = Float.parseFloat("15.5");
+            String updatedStatus = "Connection stable";
+            if(Ping <= 0){
+                updatedStatus = "Connection unavailable";
+            }
+            INetworkStatus status = retrieveNetworkStatuses().getResponseObject().get(0);
+            status.setValue(updatedStatus);
+            networkStatusRepository.update(status);
+
+            //Update Ping
+            String updatedPing = "15";
+            INetworkStatus ping = retrieveNetworkStatuses().getResponseObject().get(3);
+            ping.setValue(updatedPing);
+            networkStatusRepository.update(ping);
+
+            //Update Date
+            String updatedDate = java.time.LocalDate.now().toString().replace("-", ".");
+            INetworkStatus date = retrieveNetworkStatuses().getResponseObject().get(4);
+            date.setValue(updatedDate);
+            networkStatusRepository.update(date);
+        } catch (Exception e) {
+            response.addError("Network status", e.toString());
+        }
+
+        return response;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ServiceResponse<List<? extends IKitStatus>> retrieveKitStatuses() {
+        ServiceResponse<List<? extends IKitStatus>> response = new ServiceResponse<>();
+        try {
+            List<? extends IKitStatus> kitStatuses = kitStatusRepository.findAll(KitStatus.class);
+            response.setResponseObject(kitStatuses);
+
+        } catch (Exception ex) {
+            response.addError("", ex.getMessage());
+        }
+        return response;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ServiceResponse<List<? extends IKitStatus>> updateKitStatuses() {
+        ServiceResponse<List<? extends IKitStatus>> response = new ServiceResponse<>();
+        try {
+            Process p = Runtime.getRuntime().exec("sh update.sh");
+            p.waitFor();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader errorReader = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            line = "";
+            while ((line = errorReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            String updatedDate = java.time.LocalDate.now().toString().replace("-", ".");
+            IKitStatus kitStatusDate = retrieveKitStatuses().getResponseObject().get(2);
+            kitStatusDate.setValue(updatedDate);
+            kitStatusRepository.update(kitStatusDate);
+        } catch (Exception e) {
+            response.addError("Kit update", e.toString());
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ServiceResponse<List<? extends IDatabaseStatus>> retrieveDatabaseStatuses() {
+        ServiceResponse<List<? extends IDatabaseStatus>> response = new ServiceResponse<>();
+        try {
+            List<? extends IDatabaseStatus> databaseStatuses = databaseStatusRepository.findAll(DatabaseStatus.class);
+            response.setResponseObject(databaseStatuses);
+
+        } catch (Exception ex) {
+            response.addError("", ex.getMessage());
+        }
+        return response;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ServiceResponse<List<? extends IDatabaseStatus>> updateDatabaseStatuses() {
+        ServiceResponse<List<? extends IDatabaseStatus>> response = new ServiceResponse<>();
+        //TODO: Do some more robust error checking
+        String[] cmd = new String[]{"/bin/bash", "femr.sh"};
+        try {
+            Process pr = Runtime.getRuntime().exec(cmd, null, new File("/Users/yashsatyavarpu/Documents/super-femr/app/femr/util/backup"));
+            String updated_date = java.time.LocalDate.now().toString().replace("-", ".");
+            DatabaseStatus databaseStatus = new DatabaseStatus();
+            databaseStatus.setName("Last Backup");
+            databaseStatus.setValue(updated_date);
+            databaseStatusRepository.update(databaseStatus);
+        } catch (IOException e) {
+            response.addError("Database update", e.toString());
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+}
+
---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# todo: stop and start mysql without using sudo
+
+# Stop femr app by killing first PID shown
+#kill $(ps aux | grep java | grep -v 'grep' | awk 'NR==1{print $2}')
+
+## stop mysql
+#sudo -S launchctl unload -F /Library/LaunchDaemons/com.oracle.oss.mysql.mysqld.plist
+
+## get the latest updated images. This is assuming femr developers
+## pushed updated docker image to remote docker registry.
+#docker-compose pull
+
+docker-compose down
+
+## Builds, (re)creates, starts, and attaches to containers
+docker-compose up
+
+## start mysql
+#sudo -S launchctl load -F /Library/LaunchDaemons/com.oracle.oss.mysql.mysqld.plist
